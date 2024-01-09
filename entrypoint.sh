ba5eb62bc0b574396a7ed51f32fd20b6076c0905
Leonardo Brito <lbrito@gmail.com>
2024-01-09 14:54:14 -0800

    Refactor setup/teardown steps.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+apk add expect
+
 if [ -n "$INPUT_PATH" ]; then
   # Allow user to change directories in which to run Fly commands.
   cd "$INPUT_PATH" || exit
@@ -31,42 +33,65 @@ if ! echo "$app" | grep "$PR_NUMBER"; then
   exit 1
 fi
 
-# PR was closed - remove the Fly app if one exists and exit.
-if [ "$EVENT_TYPE" = "closed" ]; then
+teardown() {
+  # Detach app from postgres cluster and database
+  expect -c "
+    spawn flyctl postgres detach \"$INPUT_POSTGRES\" --app \"$app\"
+    expect {Select the attachment that you would like to detach*}
+    send -- \"\r\"
+    expect eof
+  "
+
   flyctl apps destroy "$app" -y || true
+}
+
+setup() {
+  # Deploy the Fly app, creating it first if needed.
+  if ! flyctl status --app "$app"; then
+    # Backup the original config file since 'flyctl launch' messes up the [build.args] section
+    cp "$config" "$config.bak"
+    flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org" --dockerfile "$dockerfile" -y
+    # Restore the original config file
+    cp "$config.bak" "$config"
+  fi
+  if [ -n "$INPUT_SECRETS" ]; then
+    echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
+  fi
+
+  # Attach postgres cluster to the app if specified.
+  if [ -n "$INPUT_POSTGRES" ]; then
+    # Create postgres app if it does not already exist
+    if ! flyctl status --app "$INPUT_POSTGRES"; then
+      flyctl postgres create --name "$INPUT_POSTGRES" --region "$region" --org "$org" --vm-size shared-cpu-1x --volume-size 1 --initial-cluster-size 1 || true
+    fi
+
+    flyctl postgres attach "$INPUT_POSTGRES" --app "$app" || true
+  fi
+
+  # Trigger the deploy of the new version.
+  echo "Contents of config $config file: " && cat "$config"
+  if [ -n "$INPUT_VM" ]; then
+    flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE"
+  else
+    flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "$INPUT_MEMORY"
+  fi
+
+  # Make some info available to the GitHub workflow.
+  flyctl status --app "$app" --json >status.json
+  hostname=$(jq -r .Hostname status.json)
+  appid=$(jq -r .ID status.json)
+  echo "hostname=$hostname" >> $GITHUB_OUTPUT
+  echo "url=https://$hostname" >> $GITHUB_OUTPUT
+  echo "id=$appid" >> $GITHUB_OUTPUT
+  echo "name=$app" >> $GITHUB_OUTPUT
+}
+
+
+if [ "$EVENT_TYPE" = "closed" ]; then
+  # PR was closed - remove the Fly app if one exists and exit.
+  teardown
   exit 0
-fi
-
-# Deploy the Fly app, creating it first if needed.
-if ! flyctl status --app "$app"; then
-  # Backup the original config file since 'flyctl launch' messes up the [build.args] section
-  cp "$config" "$config.bak"
-  flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org" --dockerfile "$dockerfile" -y
-  # Restore the original config file
-  cp "$config.bak" "$config"
-fi
-if [ -n "$INPUT_SECRETS" ]; then
-  echo $INPUT_SECRETS | tr " " "\n" | flyctl secrets import --app "$app"
-fi
-
-# Attach postgres cluster to the app if specified.
-if [ -n "$INPUT_POSTGRES" ]; then
-  flyctl postgres attach "$INPUT_POSTGRES" --app "$app" || true
-fi
-
-# Trigger the deploy of the new version.
-echo "Contents of config $config file: " && cat "$config"
-if [ -n "$INPUT_VM" ]; then
-  flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-size "$INPUT_VMSIZE"
 else
-  flyctl deploy --config "$config" --app "$app" --region "$region" --image "$image" --strategy immediate --ha=$INPUT_HA --vm-cpu-kind "$INPUT_CPUKIND" --vm-cpus $INPUT_CPU --vm-memory "$INPUT_MEMORY"
+  # PR was open or updated - deploy the server
+  setup
 fi
-
-# Make some info available to the GitHub workflow.
-flyctl status --app "$app" --json >status.json
-hostname=$(jq -r .Hostname status.json)
-appid=$(jq -r .ID status.json)
-echo "hostname=$hostname" >> $GITHUB_OUTPUT
-echo "url=https://$hostname" >> $GITHUB_OUTPUT
-echo "id=$appid" >> $GITHUB_OUTPUT
-echo "name=$app" >> $GITHUB_OUTPUT
