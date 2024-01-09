67163704c0fdfa4b1f6629896fd0c1329783432b
Leonardo Brito <lbrito@gmail.com>
2024-01-09 10:33:54 -0800

    export path

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,8 +40,10 @@ fi
 if ! flyctl status --app "$app"; then
   # Backup the original config file since 'flyctl launch' messes up the [build.args] section
   cp "$config" "$config.bak"
-  echo "fly launch PATH=$PATH"
-  PATH=/opt/hostedtoolcache/Ruby/3.2.2/x64/bin:/opt/hostedtoolcache/node/18.19.0/x64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
+  echo "fly launch before PATH=$PATH"
+  export PATH=/opt/hostedtoolcache/Ruby/3.2.2/x64/bin:/opt/hostedtoolcache/node/18.19.0/x64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+  echo "fly launch after PATH=$PATH"
+  flyctl launch --no-deploy --copy-config --name "$app" --image "$image" --region "$region" --org "$org"
   # Restore the original config file
   cp "$config.bak" "$config"
 fi
